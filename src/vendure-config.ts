91bdd19aed0f8d41d18aef78fdc56cc22af2769b
florisb <floris@modernmediahub.nl>
2024-11-19 11:03:45 +0100

    update config

--- src/vendure-config.ts
+++ src/vendure-config.ts
@@ -19,60 +19,58 @@ export const config: VendureConfig = {
         port: +(process.env.PORT || 3000),
         adminApiPath: 'admin-api',
         shopApiPath: 'shop-api',
-        // The following options are useful in development mode,
-        // but are best turned off for production for security
-        // reasons.
-        ...(IS_DEV ? {
-            adminApiPlayground: {
-                settings: { 'request.credentials': 'include' } as any,
-            },
-            adminApiDebug: true,
-            shopApiPlayground: {
-                settings: { 'request.credentials': 'include' } as any,
-            },
-            shopApiDebug: true,
-        } : {}),
+        ...(IS_DEV
+            ? {
+                adminApiPlayground: { settings: { 'request.credentials': 'include' } },
+                adminApiDebug: true,
+                shopApiPlayground: { settings: { 'request.credentials': 'include' } },
+                shopApiDebug: true,
+            }
+            : {}),
+        cors: {
+            origin: [process.env.FRONTEND_URL],
+            credentials: true,
+        },
     },
     authOptions: {
         tokenMethod: ['bearer', 'cookie'],
         superadminCredentials: {
             identifier: process.env.SUPERADMIN_USERNAME,
             password: process.env.SUPERADMIN_PASSWORD,
-            // emailAddress: 'superadmin@example.com', // Add a valid email here
-
         },
         cookieOptions: {
+            name: {
+                shop: 'shop_session',
+                admin: 'admin_session',
+            },
             secret: process.env.COOKIE_SECRET,
+            path: '/',
+            sameSite: !IS_DEV ? 'none' : 'lax',
+            secure: !IS_DEV,
+            secureProxy: !IS_DEV,
+            httpOnly: true,
+            signed: true,
+            overwrite: true,
         },
+
     },
     dbConnectionOptions: {
         type: 'postgres',
-        // See the README.md "Migrations" section for an explanation of
-        // the `synchronize` and `migrations` options.
         synchronize: false,
         migrations: [path.join(__dirname, './migrations/*.+(ts|js)')],
         logging: false,
-        database: process.env.DB_NAME,
-        schema: process.env.DB_SCHEMA,
-        host: process.env.DB_HOST,
-        url: process.env.DB_URL,
-        port: +process.env.DB_PORT,
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        ssl: process.env.DB_CA_CERT ? {
-            ca: process.env.DB_CA_CERT,
-        } : undefined,
+        url: process.env.DATABASE_URL,
+        ssl: process.env.DB_CA_CERT
+            ? {
+                ca: process.env.DB_CA_CERT,
+            }
+            : undefined,
     },
     paymentOptions: {
         paymentMethodHandlers: [dummyPaymentHandler],
     },
-    // When adding or altering custom field definitions, the database will
-    // need to be updated. See the "Migrations" section in README.md.
     customFields: {
-        Product: [{
-            name: 'test',
-            type: 'string',
-        }]
+        Product: [{ name: 'test', type: 'string' }],
     },
     plugins: [
         ReviewsPlugin,
@@ -83,40 +81,35 @@ export const config: VendureConfig = {
         AssetServerPlugin.init({
             route: 'assets',
             assetUploadDir: process.env.ASSET_UPLOAD_DIR || path.join(__dirname, '../static/assets'),
-            // If the MINIO_ENDPOINT environment variable is set, we'll use
-            // Minio as the asset storage provider. Otherwise, we'll use the
-            // default local provider.
-            storageStrategyFactory: process.env.MINIO_ENDPOINT ?  configureS3AssetStorage({
-                bucket: 'vendure-assets',
-                credentials: {
-                    accessKeyId: process.env.MINIO_ACCESS_KEY,
-                    secretAccessKey: process.env.MINIO_SECRET_KEY,
-                },
-                nativeS3Configuration: {
-                    endpoint: process.env.MINIO_ENDPOINT,
-                    forcePathStyle: true,
-                    signatureVersion: 'v4',
-                    // The `region` is required by the AWS SDK even when using MinIO,
-                    // so we just use a dummy value here.
-                    region: 'eu-west-1',
-                },
-            }) : undefined,
+            storageStrategyFactory: process.env.MINIO_ENDPOINT
+                ? configureS3AssetStorage({
+                    bucket: 'vendure-assets',
+                    credentials: {
+                        accessKeyId: process.env.MINIO_ACCESS_KEY,
+                        secretAccessKey: process.env.MINIO_SECRET_KEY,
+                    },
+                    nativeS3Configuration: {
+                        endpoint: process.env.MINIO_ENDPOINT,
+                        forcePathStyle: true,
+                        signatureVersion: 'v4',
+                        region: 'eu-west-1',
+                    },
+                })
+                : undefined,
         }),
         DefaultJobQueuePlugin.init({ useDatabaseForBuffer: true }),
         DefaultSearchPlugin.init({ bufferUpdates: false, indexStockStatus: true }),
         EmailPlugin.init({
-            devMode: true,
+            devMode: IS_DEV,
             outputPath: path.join(__dirname, '../static/email/test-emails'),
             route: 'mailbox',
             handlers: defaultEmailHandlers,
             templatePath: path.join(__dirname, '../static/email/templates'),
             globalTemplateVars: {
-                // The following variables will change depending on your storefront implementation.
-                // Here we are assuming a storefront running at http://localhost:8080.
                 fromAddress: '"example" <noreply@example.com>',
-                verifyEmailAddressUrl: 'http://localhost:8080/verify',
-                passwordResetUrl: 'http://localhost:8080/password-reset',
-                changeEmailAddressUrl: 'http://localhost:8080/verify-email-address-change'
+                verifyEmailAddressUrl: `${process.env.FRONTEND_URL}/verify`,
+                passwordResetUrl: `${process.env.FRONTEND_URL}/password-reset`,
+                changeEmailAddressUrl: `${process.env.FRONTEND_URL}/verify-email-address-change`,
             },
         }),
         AdminUiPlugin.init({
@@ -128,13 +121,3 @@ export const config: VendureConfig = {
         }),
     ],
 };
-
-// bootstrap(config).then(async (app) => {
-//     const connection: Connection = app.get(Connection);
-//     await connection
-//         .getRepository('Administrator')
-//         .update({ identifier: process.env.SUPERADMIN_USERNAME }, { emailAddress: 'superadmin@example.com' });
-//     console.log('Superadmin email address set to superadmin@example.com');
-// }).catch(err => {
-//     console.error(err);
-// });
